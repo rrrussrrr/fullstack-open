d88a0259b23b7c9a364b2f2aeb26ab3ff6c9d612
rrrussrrr <rruebner@gmail.com>
2022-09-27 21:17:29 -0400

    playing around with react

---
+++ part1/src/App.js
@@ -0,0 +1,20 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="dave"/>
+      <Hello name="f"/>
+      <Hello />
+    </div>
+  )
+}
+
+export default App
