a053fae69b734416cce0d812f98576f06b55efb2
rrrussrrr <rruebner@gmail.com>
2022-09-27 22:11:29 -0400

    refactored to split content into parts

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,9 +5,21 @@ const Header = (props) => {
 }
 const Content = (props) => {
   return (
-    <p>{props.course} {props.exercises}</p>
+    <div>
+      <Part course={props.course1} exercises={props.exercises1}/>
+      <Part course={props.course2} exercises={props.exercises2}/>
+      <Part course={props.course3} exercises={props.exercises3}/>
+    </div>
+
   )
 }
+
+const Part = (props)=>{
+  return (
+  <p>{props.course} {props.exercises}</p>
+  )
+}
+
 const Total = (props) => {
   return (
     <p>{props.text} {props.total}</p>
@@ -28,9 +40,8 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content course={part1} exercises={exercises1}/>
-      <Content course={part2} exercises={exercises2}/>
-      <Content course={part3} exercises={exercises3}/>
+      <Content course1={part1} exercises1={exercises1} course2={part2} course3={part3}
+        exercises2={exercises2} exercises3={exercises3}/>
       <Total text= "Number of exercises: " total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
