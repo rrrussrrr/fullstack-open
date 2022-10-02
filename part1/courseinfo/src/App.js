4a1459766723125dcc604e4796746933bcbd6f79
rrrussrrr <rruebner@gmail.com>
2022-10-02 18:13:02 -0400

    playing with buttons

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,15 +3,23 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
 
   console.log('rendering...', counter)
 
   return (
+    <div>
     <div>{counter}</div>
+    <button onClick={() => setCounter(counter + 1)}>plus</button>
+    <button onClick={() => setCounter(0)}>reset</button>
+    </div>
   )
 }
 
