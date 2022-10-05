19369422d68cf5f11e5ce051c2da7a8f4c8ef4fb
rrrussrrr <rruebner@gmail.com>
2022-10-05 17:32:33 -0400

    made course component

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -1,21 +1,70 @@
 import Note from './components/Note'
-
-
-
-
-const App = ({notes}) => {
-  
+const Header = (props) => {
 
   return (
+    <h1>{props.name}</h1>
+  )
+}
+const Content = (props) => {
+  return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-      {notes.map(note => 
-        <Note key={note.id} note={note}/>
-        )}
-      </ul>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
+    </div>
+
+  )
+}
+
+const Part = (props)=>{
+  return (
+  <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts.reduce((acc, cur) => {return acc + cur.exercises},0);
+  return (
+    <p>Total exercises: {total}</p>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+
 
 export default App
