f745aa0432bfd0969d84c9a00cb165166881e766
rrrussrrr <rruebner@gmail.com>
2022-10-06 19:56:49 -0400

    now gets contact info from json server using axion

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios'
+
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
