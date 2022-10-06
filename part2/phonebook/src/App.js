63a4d4db54d33bcbb0f01e9566c573c570ac72aa
rrrussrrr <rruebner@gmail.com>
2022-10-06 19:57:05 -0400

    now gets contact info from json server using axion

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import ContactsDisplay from './components/ContactsDisplay'
 import NewContactForm from './components/NewContactForm'
@@ -7,15 +8,22 @@ import NewContactForm from './components/NewContactForm'
 
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [contacts, setContacts] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/contacts')
+      .then(response => {
+        console.log('promise fulfilled')
+        setContacts(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   const addContact = (e) => {
     e.preventDefault()
