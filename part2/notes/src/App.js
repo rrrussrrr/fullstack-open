c5c761455251148983908c0961f25e23812684a5
rrrussrrr <rruebner@gmail.com>
2022-10-06 13:50:18 -0400

    made adding notes work

---
+++ part2/notes/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import Note from './components/Note'
+
+
+const App = (props) => {
+
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+
+  const addNote = (e) => {
+    e.preventDefault()
+    console.log('button clicked', e.target)
+
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+          <input 
+            value={newNote}
+            onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
