19ae513b7e2d1b25f79b05b4968a18630a4aef6c
rrrussrrr <rruebner@gmail.com>
2022-10-15 22:20:26 -0400

    validating number inputs

--- part3/phonebook-backend/models/contact.js
+++ part3/phonebook-backend/models/contact.js
@@ -15,11 +15,6 @@ mongoose.connect(url)
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    validate: {
-      validator: function(v) {
-        return /(\d{2,3})-?(\d{6,})/
-      }
-    },
     minLength: 3,
     required: true
   },
@@ -28,7 +23,7 @@ const contactSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function(v) {
-        return /\d{3}-\d{3}-\d{4}/.test(v);
+        return /(\d{2,3})-?(\d{6,})/.test(v);
       },
       message: props => `${props.value} is not a valid phone number!`
     },
